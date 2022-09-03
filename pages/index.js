a64337b5e16b46adb04fd4f5c8e450ef2fec5685
Boboye-Ak <boboye771@gmail.com>
2022-09-03 15:22:43 +0100

    Added handleSignup function

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,16 @@ import Head from "next/head"
 import { ConnectButton } from "web3uikit"
 import { useMoralis } from "react-moralis"
 import { useState } from "react"
+import axios from "axios"
 
 export default function Home() {
-    const {account, isWeb3Enabled} = useMoralis()
-    const [userData, setUserData]=useState({account:null, username:null, favNumber:null})
-    const [isLoggedIn, setIsLoggedIn]=useState(false)
+    const { account, isWeb3Enabled } = useMoralis()
+    const [userData, setUserData] = useState({ account: null, username: null, favNumber: null })
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [newUsername, setNewUsername] = useState("")
+    const [newFavNumber, setNewFavNumber] = useState("")
+
+    const handleSignUp = () => {}
     return (
         <div>
             <Head>
@@ -18,6 +23,43 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <ConnectButton moralisAuth={false} />
+            {account ? (
+                <div>
+                    {isLoggedIn ? (
+                        <div>
+                            You are logged in as {userData.username}({userData.account}) and your
+                            favourite number is {userData.favNumber}
+                        </div>
+                    ) : (
+                        <div>
+                            Sign Up Here
+                            <form>
+                                <label for="username">username:</label>
+                                <input
+                                    type="text"
+                                    id="username"
+                                    value={newUsername}
+                                    onChange={(e) => {
+                                        setNewUsername(e.target.value)
+                                    }}
+                                ></input>
+                                <label for="fav-number">fav-number:</label>
+                                <input
+                                    type="number"
+                                    id="fav-number"
+                                    value={newFavNumber}
+                                    onChange={(e) => {
+                                        setNewFavNumber(e.target.value)
+                                    }}
+                                ></input>
+                                <button onClick={handleSignUp}>SIGN UP</button>
+                            </form>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <div>Please Connect your wallet</div>
+            )}
         </div>
     )
 }
