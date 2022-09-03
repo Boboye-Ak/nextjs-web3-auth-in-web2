d125800297f944f14168dbe768394c5d9b766af8
Boboye-Ak <boboye771@gmail.com>
2022-09-03 02:17:43 +0100

    Imported useMoralis hook and added state variables

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import { ConnectButton } from "web3uikit"
+import { useMoralis } from "react-moralis"
+import { useState } from "react"
 
 export default function Home() {
+    const {account, isWeb3Enabled} = useMoralis()
+    const [userData, setUserData]=useState({account:null, username:null, favNumber:null})
+    const [isLoggedIn, setIsLoggedIn]=useState(false)
     return (
         <div>
             <Head>
-                <title>Create Next App</title>
-                <meta name="web3 auth in web2" content="using connect wallet to login to a web2 app" />
+                <title>Web3 auth in Web2</title>
+                <meta
+                    name="web3 auth in web2"
+                    content="using connect wallet to login to a web2 app"
+                />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <ConnectButton moralisAuth={false} />
         </div>
     )
 }
